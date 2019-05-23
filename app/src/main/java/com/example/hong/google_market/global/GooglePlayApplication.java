d9a1919c433f03999f040f8715b56eb75981f23f
zyh <289590351@qq.com>
2019-05-23 13:55:59 +0800

    仅仅创建了一个Application测试一下

---
+++ app/src/main/java/com/example/hong/google_market/global/GooglePlayApplication.java
@@ -0,0 +1,35 @@
+package com.example.hong.google_market.global;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+/**
+ * Created by hong on 2019/5/23.
+ */
+
+public class GooglePlayApplication extends Application {
+    private static Context context;
+    private static Handler handler;
+    private static int mainThreadId;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        handler = new Handler();
+        mainThreadId = android.os.Process.myTid();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    public static Handler getHandler() {
+        return handler;
+    }
+
+    public static int getMainThreadId() {
+        return mainThreadId;
+    }
+}
